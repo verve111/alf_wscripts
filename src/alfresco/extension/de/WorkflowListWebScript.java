13a9cf17d81a07a56187c53c2de442e00b3d9498
verve111 <verve111@mail.ru>
2014-05-05 13:12:27 +0200

    simple lucene query

--- src/alfresco/extension/de/WorkflowListWebScript.java
+++ src/alfresco/extension/de/WorkflowListWebScript.java
@@ -8,17 +8,24 @@ import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.ResultSetRow;
 import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.cmr.security.PermissionService;
 import org.springframework.extensions.webscripts.DeclarativeWebScript;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
-public class WorkflowListWebScript extends DeclarativeWebScript  {
+public class WorkflowListWebScript extends DeclarativeWebScript {
 
-    private NodeService nodeService;
-    private SearchService searchService;
-    
-    public void setNodeService(NodeService nodeService) {
+	private NodeService nodeService;
+	private SearchService searchService;
+	private PermissionService permissionService;
+
+	public void setPermissionService(PermissionService permissionService) {
+		this.permissionService = permissionService;
+	}
+
+	public void setNodeService(NodeService nodeService) {
 		this.nodeService = nodeService;
 	}
 
@@ -27,25 +34,33 @@ public class WorkflowListWebScript extends DeclarativeWebScript  {
 	}
 
 	public WorkflowListWebScript() {
-        super();
-    }
-    
-	@Override
-    protected Map<String, Object> executeImpl(WebScriptRequest request,
-            Status status) {
-		AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
-        StoreRef storeRef = new StoreRef(StoreRef.PROTOCOL_WORKSPACE, "SpacesStore");
-        ResultSet resultSet = searchService.query(storeRef, SearchService.LANGUAGE_LUCENE, "PATH:\"/app:company_home\"/* TYPE:\"cm:content\"");
-        System.out.println(resultSet.length());
-        NodeRef companyHome = resultSet.getNodeRef(0);
-        resultSet.close();
-        if (companyHome != null) {
-        	
-        }
-        System.out.println(companyHome);
+		super();
+	}
 
-        Map<String, Object> result = new LinkedHashMap<String, Object>();
-        return result;
+	@Override
+	protected Map<String, Object> executeImpl(WebScriptRequest request, Status status) {
+		AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
+		StoreRef storeRef = new StoreRef(StoreRef.PROTOCOL_WORKSPACE, "SpacesStore");
+		ResultSet resultSet = searchService.query(storeRef, SearchService.LANGUAGE_LUCENE, 
+				"PATH:\"/app:company_home//*\" AND TYPE:\"cm:content\" AND @cm\\:name:\"Att1\"");
+
+		// folders 82
+		// content 187
+		// "PATH:\"/app:company_home//*\" AND TYPE:\"cm:content\" AND @cm\\:name:\"Att1\""
+		System.out.println("length: " + resultSet.length());
+		if (resultSet.length() < 20) {
+			for (ResultSetRow row : resultSet) {
+				//System.out.println(row.getNodeRef());
+			}
+		}
+		if (resultSet.length() > 0) {
+			NodeRef nr = resultSet.getNodeRef(0);
+			System.out.println("currnode: " + nr);
+			permissionService.getAllSetPermissions(nr);
+		}
+		resultSet.close();
+		Map<String, Object> result = new LinkedHashMap<String, Object>();
+		return result;
 	}
 
 }
